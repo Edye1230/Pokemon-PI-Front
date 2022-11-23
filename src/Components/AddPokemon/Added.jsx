40437f637b260d93650dbb315e74993548c78e79
Edye1230 <edye.1230@gmail.com>
2022-11-23 01:37:28 -0500

    add fixed

--- src/Components/AddPokemon/Added.jsx
+++ src/Components/AddPokemon/Added.jsx
@@ -5,8 +5,8 @@ const Added = () => {
     return (
         <div id="addpokemon">
             <h1>Pokemon añadido correctamente</h1>
-            <Link id="landing-btn" to={"/addpokemon"}>¿Añadir otro?</Link> <br />
-            <Link id="landing-btn" to={"/home"}>Ir al listado</Link>
+            <Link id="landing-btn" to="/addpokemon">¿Añadir otro?</Link> <br />
+            <a id="landing-btn" href="/home">Ir al listado</a>
         </div>
     )
 }
